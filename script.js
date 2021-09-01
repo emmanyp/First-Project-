c3d4af5066bfe10db3830ed18addde14fa848b16
emmanyp <emma.ronaldo7@gmail.com>
2021-09-01 06:59:57 -0400

    save

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
+let IMAGES = {
+    
+}
+
 const myQuestions = [
     {
       question: 'Which character has a twin?',
@@ -91,43 +95,10 @@ const questionContainerElement = document.getElementById('question-container')
 const questionElement = document.getElementById('question')
 const answerButtonsElement = document.getElementById('answer-buttons')
 
-  class currentQuestion {
-    constructor() {
-        this.index = index
-        this.question = question
-    }
-}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// for (let i = 0; i < myQuestions.length; i++) {
-//    let response = window.prompt(myQuestions[i].question)
-//    if (response === answers) {
-//        score++;
-//        alert('correct!')
-//    } else {
-//        alert(wrong)
-//    }
-    
-// }
   
-  let shuffledQuestions;
 let currentQuestionIndex;
 
 startButton.addEventListener('click', startGame)
@@ -135,10 +106,16 @@ nextButton.addEventListener('click', () => {
   currentQuestionIndex++
   setNextQuestion()
 })
-
+let shuffledQuestions = [] 
 function startGame() {
   startButton.classList.add('hide')
-  shuffledQuestions = myQuestions.sort(() => Math.random() - .5)
+
+    while (shuffledQuestions.length <= 9) {
+        const random = myQuestions[Math.floor(Math.random() * myQuestions.length)]
+        if (!shuffledQuestions.includes(random)) {
+            shuffledQuestions.push(random)
+        }
+    }
   currentQuestionIndex = 0
   questionContainerElement.classList.remove('hide')
   setNextQuestion()
@@ -150,9 +127,18 @@ function setNextQuestion() {
 }
 
 function showQuestion(question) {
-  questionElement.innerText = question.question
-  
-}
+    questionElement.innerText = question.question
+    question.answers.forEach(answer => {
+      const button = document.createElement('button')
+      button.innerText = answer.text
+      button.classList.add('btn')
+      if (answer.correct) {
+        button.dataset.correct = answer.correct
+      }
+      button.addEventListener('click', selectAnswer)
+      answerButtonsElement.appendChild(button)
+    })
+  }
 
 function resetState() {
   nextButton.classList.add('hide')
@@ -163,23 +149,78 @@ function resetState() {
 
 
 
+function selectAnswer(e) {
+    const selectedButton = e.target
+    const correct = selectedButton.dataset.correct
+    setStatusClass(document.body, correct)
+    Array.from(answerButtonsElement.children).forEach(button => {
+      setStatusClass(button, button.dataset.correct)
+    })
+    if (shuffledQuestions.length > currentQuestionIndex + 1) {
+      nextButton.classList.remove('hide')
+    } else {
+      startButton.innerText = 'Restart'
+      startButton.classList.remove('hide')
+    }
+  }
+  
+  function setStatusClass(element, correct) {
+    clearStatusClass(element)
+    if (correct) {
+      element.classList.add('correct')
+    } else {
+      element.classList.add('wrong')
+    }
+  }
+  
+  function clearStatusClass(element) {
+    element.classList.remove('correct')
+    element.classList.remove('wrong')
+  }
 
-// function checkAnswer()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//      question.question = myQuestions[currentQuestionIndex];
+//     if (answers.text === question.answers) {
+//         score++;
+//     }
+
+//     currentQuestion++;
+
+//     if (currentQuestionIndex >= myQuestions.length) {
+//         showSummary();
+//     } else {
+//         showQuestion();
+//     }
+//     showQuestion();
+
 
 // function setStatusClass(element, correct) {
-//   clearStatusClass(element)
-//   if (correct) {
-//     element.classList.add('correct')
-//   } else {
-//     element.classList.add('wrong')
+//     clearStatusClass(element)
+//     if (correct) {
+//       element.classList.add('correct')
+//     } else {
+//       element.classList.add('wrong')
+//     }
 //   }
-// }
-
-// function clearStatusClass(element) {
-//   element.classList.remove('correct')
-//   element.classList.remove('wrong')
-// }
-
- 
   
+//   function clearStatusClass(element) {
+//     element.classList.remove('correct')
+//     element.classList.remove('wrong')
+//   }
   
